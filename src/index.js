7d7c9950ab4c221cc1528f32f26eb9d225080aeb
scjang <scjang@scjangui-MacBook-Pro.local>
2017-06-16 11:29:07 +0900

    tree shaking for lodash

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import _ from 'lodash';
 
 function component () {
   var element = document.createElement('div');
+  var temp = undefined;
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // test for tree shaking isEmpty function
+  console.log(_.isEmpty(temp));
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -5,5 +8,28 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin({
+      collections: true,
+      paths: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      beautify: true
+    })
+  ],
+  module: {
+    rules: [{
+      use: [{
+        loader: "babel-loader",
+        options: {
+          plugins: ['lodash'],
+          presets: [['env', { 'modules': false, 'targets': { 'node': 4 } }]]
+        }
+      }],
+      test: /\.js$/,
+      exclude: /node_modules/,
+    }]
   }
 };
